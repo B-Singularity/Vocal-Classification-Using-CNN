a930227e5b01309fcb5bc3343eb91015c8f7caf3
B-singularity <skil1489@naver.com>
2024-09-20 15:56:33 +0900

    feat:create params impl: q quit ''

--- src/nas/macro_controller.py
+++ src/nas/macro_controller.py
@@ -1,13 +1,16 @@
 from nas.controller import Controller
-
+import torch.nn.init as init
+import torch.nn as nn
+import torch
 
 class MacroController(Controller):
     def __init__(self,
+                 search_whole_channels=False,
                  num_layers=4,
                  num_branches=6,
                  out_filters=48,
                  lstm_size=32,
-                 lstm_layers=2,
+                 lstm_num_layers=2,
                  lstm_keep_prob=1.0,
                  tanh_constant=None,
                  temperature=None,
@@ -28,13 +31,13 @@ class MacroController(Controller):
                  skip_weight=0.5,
                  *args,
                  **kwargs):
-
+        self.search_whole_channels = search_whole_channels
         self.num_layers = num_layers
         self.num_branches = num_branches
         self.out_filters = out_filters
 
         self.lstm_size = lstm_size
-        self.lstm_layers = lstm_layers
+        self.lstm_num_layers = lstm_num_layers
         self.lstm_keep_prob = lstm_keep_prob
         self.tanh_constant = tanh_constant
         self.temperature = temperature
@@ -59,6 +62,44 @@ class MacroController(Controller):
 
         self._create_params()
         self._build_sample()
+
+    def _uniform_initializer(self, tensor, minval=-0.1, maxval=0.1):
+        return init.uniform_(tensor, minval, maxval)
+
+    def _create_params(self):
+        with torch.no_grad():
+            self.w_lstm = []
+            for layer_id in range(self.lstm_num_layers):
+                w = torch.empty(2 * self.lstm_size, 4 * self.lstm_size)
+                self._uniform_initializer(w, minval=-0.1, maxval=0.1)
+                self.w_lstm.append(w)
+
+            self.g_emb = torch.empty(1, self.lstm_size)
+            self._uniform_initializer(self.g_emb, minval=-0.1, maxval=0.1)
+
+            if self.search_whole_channels:
+                self.w_emb = torch.empty(self.num_branches, self.lstm_size)
+                self._uniform_initializer(self.w_emb, minval=-0.1, maxval=0.1)
+                self.w_soft = torch.empty(self.lstm_size, self.num_branches)
+                self._uniform_initializer(self.w_soft, minval=-0.1, maxval=0.1)
+            else:
+                self.w_emb = {"start": [], "count": []}
+                for branch_id in range(self.num_branches):
+                    w_start = torch.empty(self.out_filters, self.lstm_size)
+                    w_count = torch.empty(self.out_filters - 1, self.lstm_size)
+                    self._uniform_initializer(w_start, minval=-0.1, maxval=0.1)
+                    self._uniform_initializer(w_count, minval=-0.1, maxval=0.1)
+                    self.w_emb["start"].append(w_start)
+                    self.w_emb["count"].append(w_count)
+
+                self.w_soft = {"start": [], "count": []}
+                for branch_id in range(self.num_branches):
+                    w_start = torch.empty(self.lstm_size, self.out_filters)
+                    w_count = torch.empty(self.lstm_size, self.out_filters - 1)
+                    self._uniform_initializer(w_start, minval=-0.1, maxval=0.1)
+                    self._uniform_initializer(w_count, minval=-0.1, maxval=0.1)
+                    self.w_soft["start"].append(w_start)
+                    self.w_soft["count"].append(w_count)
 
 
 
